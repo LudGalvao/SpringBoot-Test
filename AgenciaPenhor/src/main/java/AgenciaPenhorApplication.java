0ef92f371a43a85e9e1999950af2deabaef8a726
Lud Galvão <99023858+LudGalvao@users.noreply.github.com>
2024-04-12 13:41:47 -0300

    Add files via upload

---
+++ AgenciaPenhor/src/main/java/AgenciaPenhorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaPenhorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaPenhorApplication.class, args);
+	}
+
+}
